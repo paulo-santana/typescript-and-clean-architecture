71c2529b5f818b2ba65690a392db52ececc7583b
Paulo Santana <paulo.santana.r@gmail.com>
2020-08-01 09:43:11 -0300

    ensure LocalSavePurchases resolves successfully

--- src/data/usecases/save-purchases/local-save-purchases.spec.ts
+++ src/data/usecases/save-purchases/local-save-purchases.spec.ts
@@ -36,7 +36,7 @@ describe('LocalSavePurchases', () => {
     const timestamp = new Date();
     const { sut, cacheStore } = makeSut(timestamp);
     const purchases = mockPurchases();
-    await sut.save(purchases);
+    const promise = sut.save(purchases);
     expect(cacheStore.messages).toEqual([
       CacheStoreSpy.Message.delete,
       CacheStoreSpy.Message.insert,
@@ -47,6 +47,7 @@ describe('LocalSavePurchases', () => {
       timestamp,
       value: purchases
     });
+    await expect(promise).resolves.toBeFalsy();
   });
 
   test('Should throw if insert throws', async () => {
